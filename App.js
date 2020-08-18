c0457e3c348c7a731e38318e20a8129af61d83e8
Daniel Stern <daniel@danielstern.ca>
2020-08-18 14:32:32 -0400

    :ant:

--- App.js
+++ App.js
@@ -1,21 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
+import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { StyleSheet, Image } from 'react-native';
+import { HomeScreen } from './components/HomeScreen';
+import { DetailsScreen } from './components/DetailsScreen';
 
+const Stack = createStackNavigator();
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+      <NavigationContainer >
+
+          <Image source={require('./img/logo.png')} style={styles.logo} />
+
+              <Stack.Navigator>
+
+                  <Stack.Screen name="Home" component={HomeScreen}/>
+                  <Stack.Screen name="Details" component={DetailsScreen}/>
+
+              </Stack.Navigator>
+
+      </NavigationContainer>
+
   );
+
 }
 
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  logo: {
+      alignSelf: "stretch", 
+      height: 100,
+      borderBottomColor: "lightgray",
+      borderBottomWidth: StyleSheet.hairlineWidth
+  }
 });
---
+++ components/DetailsScreen/index.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
+
+export const DetailsScreen = ({route})=>{
+
+    const { name, date } = route.params.item;
+
+    return (
+
+        <View style={styles.container}>
+  
+            <Text>
+
+                <Image source={require('./../../img/img-3.jpg')} style={{height: 200, width: "100%"}}/>
+                <h1 style={{marginBottom: 10}}>
+                    {name}
+                </h1>
+                <h2>
+
+                    {date.toLocaleDateString("en-us", {day: "numeric", weekday: "long", month: "long", year:"numeric"})}
+
+                </h2> 
+                {/* <p>
+
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eget tellus fringilla, tempus magna ut, accumsan lectus. 
+                    Sed pellentesque, mi non tempor auctor, nisl sem cursus sem, sed ultricies augue metus non ex. 
+                    Morbi venenatis suscipit nunc vel facilisis. Sed sit amet dictum mauris, quis blandit velit. 
+
+                </p> */}
+
+            </Text>
+
+            <Button title="Purchase Tickets"></Button>
+
+        </View>
+    )
+
+}
+  
+
+const styles = StyleSheet.create({
+
+    container: {
+
+        backgroundColor: "white",
+        height: "100%",
+        margin: 20,
+        padding: 20
+
+    },
+    button: {
+
+        padding: 10, 
+        backgroundColor: "steelblue", fontSize: 18, color: "white", textTransform:"uppercase", borderRadius: 4
+
+    }
+})
